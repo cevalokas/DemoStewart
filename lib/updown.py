a626f0b3eed1f6a142dd4d6865130d469ef7c370
cevalokas <67790474+cevalokas@users.noreply.github.com>
2023-11-04 12:00:33 +0800

    modified:   lib/nod_pwm.py 	modified:   lib/pwm_cmd.csv 	new file:   lib/updown.py 	modified:   test/test_0.py

--- lib/updown.py
+++ lib/updown.py
@@ -15,21 +15,21 @@ def neckNod(file_path):
   refresh(file_path)
   
   while (pos >= min_pulse):
-    PWM_time = np.array([t, pos, 1500, pos, 3000 - pos, 1500, 3000 - pos])
+    PWM_time = np.array([t, pos, 3000-pos, pos, 3000-pos, pos, 3000-pos])
     PWM = np.copy(PWM_time)
     PWM[1:] = PWM[1:]*4096/20000
     writePWM(file_path, PWM)
     pos = pos - quickness
   pos = pos + quickness
   while (pos <= max_pulse):
-    PWM_time = np.array([t, pos, 1500, pos, 3000 - pos, 1500, 3000 - pos])
+    PWM_time = np.array([t, pos, 3000-pos, pos, 3000-pos, pos, 3000-pos])
     PWM = np.copy(PWM_time)
     PWM[1:] = PWM[1:]*4096/20000
     writePWM(file_path, PWM)
     pos = pos + quickness
   pos = pos + quickness
   while (pos >= mid_pulse):
-    PWM_time = np.array([t, pos, 1500, pos, 3000 - pos, 1500, 3000 - pos])
+    PWM_time = np.array([t, pos, 3000-pos, pos, 3000-pos, pos, 3000-pos])
     PWM = np.copy(PWM_time)
     PWM[1:] = PWM[1:]*4096/20000
     writePWM(file_path, PWM)
--- test/test_0.py
+++ test/test_0.py
@@ -5,5 +5,5 @@ ser = serial.Serial('COM4', 115200, 8, 'N', 1)
 # Send character 'S' to start the program
 #self.USBPort = serial.Serial('COM4', 115200, 8, 'N', 1)
 
-ser.write(bytes(b'\x01\x00\x01\x01'))
-ser.write(b'\x01\x00\x01\x01')
+ser.write(bytes(b'\x01\x00\x02\x01'))
+#ser.write(b'\x01\x00\x01\x01')
